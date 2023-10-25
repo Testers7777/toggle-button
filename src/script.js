318c2be6bd7b81b6879540736bea92327c908e82
Testers7777 <Tiagodemingo@gmail.com>
2023-10-25 15:51:43 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 const target = document.querySelector('.circle');
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-  if (target.style.marginLeft == "69px") {
-    target.style.backgroundColor = 'greenyellow'
-    target.style.marginLeft = "0px"
-  } else {
-    target.style.backgroundColor = 'crimson'
-    target.style.marginLeft = "69px"
-  }
+    if (target.style.transform == "translate(25%, 0%)") {
+        target.style.backgroundColor = 'greenyellow'
+        target.style.transform = "translate(-120%, -0%)"
+    } else {
+        target.style.backgroundColor = 'crimson'
+        target.style.transform = "translate(25%, 0%)"
+    }
 })
