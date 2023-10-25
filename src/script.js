17e18171fc91754c64b2952d50d02ee4b06cc541
Testers7777 <Tiagodemingo@gmail.com>
2023-10-25 15:54:52 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
     if (target.style.transform == "translate(25%, 0%)") {
         target.style.backgroundColor = 'greenyellow'
-        target.style.transform = "translate(-120%, -0%)"
+        target.style.transform = "translate(-123%, -0%)"
     } else {
         target.style.backgroundColor = 'crimson'
         target.style.transform = "translate(25%, 0%)"
